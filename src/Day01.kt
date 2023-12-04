044abfb7e6ecb49ca1a41be038936fe29ca39bec
Michel Drenthe <mixed84@gmail.com>
2023-12-04 11:06:03 +0100

    Day 1 - part 1 done, part 2 WIP.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,32 @@
 fun main() {
+    val digitsMap = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9",
+    )
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { line -> line.filter { c -> c.isDigit() } }
+            .sumOf { "${it.first()}${it.last()}".toInt() }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
