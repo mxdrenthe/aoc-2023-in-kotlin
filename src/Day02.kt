d5c9ee549dc1af54da13a6bf7f0c53b0a67cbf11
Michel Drenthe <mixed84@gmail.com>
2023-12-06 12:28:08 +0100

    Finish day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,55 +1,75 @@
 import com.sun.jdi.InvalidTypeException
+import kotlin.math.max
 
 fun main() {
 
-    fun part1(input: List<String>, red: Int, green: Int, blue: Int): Int {
-         return input
-                .map { it.split(": ") }
-                .map { game ->
-                    Pair(
-                        game[0].split(" ")[1].toInt(),
-                        game.subList(1, game.size)
-                            .flatMap { hands ->
-                                hands
-                                    .split("; ")
-                                    .map { hand ->
-                                        hand.split(", ")
-                                            .map { cube ->
-                                                val cubeSplit = cube.split(" ")
-                                                Pair(cubeSplit[0].toInt(), cubeSplit[1])
-                                            }
+    fun convertInput(input: List<String>) = input
+        .map { it.split(": ") }
+        .map { game ->
+            Pair(
+                game[0].split(" ")[1].toInt(),
+                game.subList(1, game.size)
+                    .flatMap { hands ->
+                        hands
+                            .split("; ")
+                            .map { hand ->
+                                hand.split(", ")
+                                    .map { cube ->
+                                        val cubeSplit = cube.split(" ")
+                                        Pair(cubeSplit[0].toInt(), cubeSplit[1])
                                     }
                             }
-                    )
-                }.filter { (_, value) ->
-                    var threshold = true
-                    value.forEach { hand ->
-                        hand.forEach { (amount, color) ->
-                            if (threshold) {
-                                threshold = when (color) {
-                                    "red" -> amount <= red
-                                    "green" -> amount <= green
-                                    "blue" -> amount <= blue
-                                    else -> throw InvalidTypeException("invalid value")
-                                }
-                            }
+                    }
+            )
+        }
+
+    fun part1(input: List<String>, red: Int, green: Int, blue: Int): Int {
+        return convertInput(input).filter { (_, value) ->
+            var threshold = true
+            value.forEach { hand ->
+                hand.forEach { (amount, color) ->
+                    if (threshold) {
+                        threshold = when (color) {
+                            "red" -> amount <= red
+                            "green" -> amount <= green
+                            "blue" -> amount <= blue
+                            else -> throw InvalidTypeException("invalid value")
                         }
                     }
-                    threshold
                 }
-                .sumOf { it.first }
+            }
+            threshold
+        }
+            .sumOf { it.first }
+    }
+
+    fun colorCompare(input: Int, currentValue: Int) = if (currentValue <= input) {
+        input
+    } else {
+        currentValue
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+       return convertInput(input).map { (_, value) ->
+            var maxColor = Triple(0, 0, 0)
+            value.forEach { hand ->
+                hand.forEach { (amount, color) ->
+                    maxColor = when (color) {
+                        "red" -> Triple(colorCompare(amount, maxColor.first), maxColor.second, maxColor.third)
+                        "green" -> Triple( maxColor.first, colorCompare(amount, maxColor.second), maxColor.third)
+                        "blue" -> Triple(maxColor.first, maxColor.second, colorCompare(amount, maxColor.third))
+                        else -> throw InvalidTypeException("invalid value")
+                    }
+                }
+            }
+            maxColor
+        }.sumOf { it.first * it.second * it.third }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput, 12, 13, 14) == 8)
-
-//    val testInput2 = readInput("Day02_test2")
-//    check(part2(testInput2) == 281)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input, 12, 13, 14).println()
